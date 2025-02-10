1f22b0cdd587077eca721b99ef7dcb6cc5ebb442
sakabmw <sakabumi.wahyudi@gmail.com>
2025-02-10 11:29:33 +0700

    add about me section, expanding card

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 document.getElementById("toggleTheme").addEventListener("click", function() {
     document.body.classList.toggle("dark-mode");
 });
+
+document.getElementById("toggleTheme").addEventListener("dblclick", function() {
+    let aboutMe = document.getElementById("aboutMe");
+    if (aboutMe.style.display === "none") {
+        aboutMe.style.display = "block";
+    } else {
+        aboutMe.style.display = "none";
+    }
+});
